79dddd5b39bcee29a00b12d5ccb825b3582c1f5f
Max Brown <mb39@sanger.ac.uk>
2021-07-25 18:16:20 +0100

    Add fw_canon.R

---
+++ fw_canon.R
@@ -0,0 +1,124 @@
+#!/usr/bin/env Rscript
+
+# Max Brown 2021; Wellcome Sanger Institute
+
+###############
+## Libraries ##
+###############
+
+library(argparse)
+library(data.table)
+
+######################
+## Argument Parsing ##
+######################
+
+parser <- ArgumentParser()
+
+parser$add_argument("-t", "--tsv", help = "input TSV file from fasta_windows output.")
+
+args <- parser$parse_args()
+
+###############
+## Functions ##
+###############
+
+# get the kmers from the colnames of out data table
+get_kmers <- function(x) {
+  colnms <- colnames(x)
+  matches <- regmatches(colnms, gregexpr("A*|G*|C*|T*", colnms))
+  matches_list <- lapply(matches, function(e) paste0(e, collapse = ""))
+
+  kmers <- unlist(matches_list)
+  kmers[kmers != ""]
+}
+
+# reverse complement a (vector of)
+# string of DNA letters
+revcomp <- function(x) {
+  # kmer assignment vector
+  kmers <- character(length(x))
+
+  # loop over vector of kmers
+  for (i in 1:length(x)) {
+    # bases assignement vector
+    vector <- character(length(kmers[1]))
+    bases <- strsplit(x[i], "")[[1]]
+
+    for (j in 1:length(bases)) {
+      # complement
+      switch(bases[j],
+        "A" = {
+          bases[j] <- "T"
+        },
+        "G" = {
+          bases[j] <- "C"
+        },
+        "C" = {
+          bases[j] <- "G"
+        },
+        "T" = {
+          bases[j] <- "A"
+        }
+      )
+      vector[j] <- bases[j]
+    }
+    # and reverse
+    kmers[i] <- paste0(rev(vector), collapse = "")
+  }
+  return(kmers)
+}
+
+# from a full set of non-canonical kmers
+# generate the set of canonical kmers
+generate_canonical_kmers <- function(x) {
+  kmers <- x
+  rev_comp_kmers <- revcomp(x)
+
+  rev_comp_kmers[!(kmers < rev_comp_kmers)]
+}
+
+# aggregate columns of our data with the same name
+agg <- function(df) {
+  do.call(cbind, lapply(split(as.list(df), names(df)), function(x) {
+    Reduce(`+`, x)
+  }))
+}
+
+##########
+## Main ##
+##########
+
+File <- args$tsv
+
+if (is.null(File)) {
+  stop("[-]\tNo input file detected.")
+}
+
+dat <- fread(File)
+
+# get kmers from our input data
+kmers <- get_kmers(dat)
+
+# rename columns
+colnames(dat)[
+  !grepl(pattern = "ID|description|start|end", x = colnames(dat))
+] <-
+  ifelse(kmers < revcomp(kmers), kmers, revcomp(kmers))
+
+# aggregate data on equivalent colnames
+# and set as DT
+dat2 <- agg(dat)
+dat2 <- setDT(as.data.frame(dat2))
+
+# generate canonical kmers and lexicographically order them
+canon_kmers <- generate_canonical_kmers(kmers)
+canon_kmers_ordered <- canon_kmers[order(canon_kmers)]
+
+# apply this to output
+setcolorder(dat2, c(colnames(dat)[
+  grepl(pattern = "ID|description|start|end", x = colnames(dat))
+], canon_kmers_ordered))
+
+# write to stdout
+fwrite(x = dat2, file = "", sep = "\t")
--- fw_group.R
+++ fw_group.R
@@ -1,28 +1,45 @@
 #!/usr/bin/env Rscript
 
-# much easier and safer to code here than in rust
-# take the file, window size, grouping size (10kb, 100kb, 1Mb, whole contig/scaff)
+# Max Brown 2021; Wellcome Sanger Institute
+
+###############
+## Libraries ##
+###############
 
 library(argparse)
 library(data.table)
 
+######################
+## Argument Parsing ##
+######################
+
 parser <- ArgumentParser()
 
-parser$add_argument("-t", "--tsv", help="input TSV file from fasta_windows output.")
-parser$add_argument("-w", "--window", type="integer", default=1000, 
-                    help="Size of window used in fasta_windows [fixed at 1000]",
-                    metavar="number")
-parser$add_argument("-g", "--group", type="integer", default=10000, 
-                    help="Size of window to group into [default 10000]",
-                    metavar="number")
-parser$add_argument("-r", "--round", type="integer", default=3, 
-                    help="Round numbers to this many decimal places. Must be < 3 [default 3]",
-                    metavar="number")
-parser$add_argument("-f", "--function", type="character", default="mean", 
-                    help="Function to apply to groups [default mean: other options are Mode, median, var, sd]",
-                    metavar="string")
-parser$add_argument("-c", "--chromosomal", action="store_true",
-                    help="Aggregate statistics at the chromosomal level?")
+parser$add_argument("-t", "--tsv", help = "input TSV file from fasta_windows output.")
+parser$add_argument("-w", "--window",
+  type = "integer", default = 1000,
+  help = "Size of window used in fasta_windows [fixed at 1000]",
+  metavar = "number"
+)
+parser$add_argument("-g", "--group",
+  type = "integer", default = 10000,
+  help = "Size of window to group into [default 10000]",
+  metavar = "number"
+)
+parser$add_argument("-r", "--round",
+  type = "integer", default = 3,
+  help = "Round numbers to this many decimal places. Must be < 3 [default 3]",
+  metavar = "number"
+)
+parser$add_argument("-f", "--function",
+  type = "character", default = "mean",
+  help = "Function to apply to groups [default mean: other options are Mode, median, var, sd]",
+  metavar = "string"
+)
+parser$add_argument("-c", "--chromosomal",
+  action = "store_true",
+  help = "Aggregate statistics at the chromosomal level?"
+)
 args <- parser$parse_args()
 
 File <- args$tsv
@@ -33,33 +50,33 @@ chromosomal <- args$chromosomal
 r <- args$round
 
 # remove scientific notation
-options(scipen=999)
+options(scipen = 999)
 
 # some parsing logic
 
-if(is.null(File)) {
+if (is.null(File)) {
   stop("[-]\tNo input file detected.")
 }
 
 # keep just for future dev potentially
-if(window_size != 1000) {
+if (window_size != 1000) {
   stop("[-]\tWindow size should be 1000bp (1kb).")
 }
 
 # could be more flexible..?
 group_size_options <- c(10000, 100000, 1000000)
 
-if(group_size %in% group_size_options == FALSE) {
+if (group_size %in% group_size_options == FALSE) {
   stop("[-]\tGroup should be either 10000 (10kb), 100000 (100kb), or 1000000 (1Mb).")
 }
 
 fun_options <- c("mean", "Mode", "median", "var", "sd")
 
-if(fun %in% fun_options == FALSE) {
+if (fun %in% fun_options == FALSE) {
   stop("[-]\tGrouping function should be either mean, Mode, median, var, or sd.")
 }
 
-if(r > 3) {
+if (r > 3) {
   stop("[-]\tr should be < 3, as the original output is capped at 3 decimal places.")
 }
 
@@ -69,83 +86,100 @@ Mode <- function(x) {
   ux[which.max(tabulate(match(x, ux)))]
 }
 
+###############
+## Functions ##
+###############
 
 calc_aggregations <- function(file, window_size, group_size, fun = c("mean", "Mode", "median", "var", "sd"), chromosomal) {
   # read in file
   windows <- fread(file = file)
   # get aggregation function
   fun <- match.fun(fun)
-  #fun_char <- as.character(substitute(fun))
+  # fun_char <- as.character(substitute(fun))
   # column names will be consistent, so subset is okay here.
   column_names <- names(windows)[4:14]
-  
-  if(chromosomal) {
-    chromosomal_table <- windows[, .(fun(get(column_names[1])), 
-                fun(get(column_names[2])),
-                fun(get(column_names[3])),
-                fun(get(column_names[4])),
-                fun(get(column_names[5])),
-                fun(get(column_names[6])),
-                fun(get(column_names[7])),
-                fun(get(column_names[8])),
-                fun(get(column_names[9])),
-                fun(get(column_names[10])),
-                fun(get(column_names[11]))),
-                by = .(ID)]
-    
+
+  if (chromosomal) {
+    chromosomal_table <- windows[, .(
+      fun(get(column_names[1])),
+      fun(get(column_names[2])),
+      fun(get(column_names[3])),
+      fun(get(column_names[4])),
+      fun(get(column_names[5])),
+      fun(get(column_names[6])),
+      fun(get(column_names[7])),
+      fun(get(column_names[8])),
+      fun(get(column_names[9])),
+      fun(get(column_names[10])),
+      fun(get(column_names[11]))
+    ),
+    by = .(ID)
+    ]
+
     setnames(chromosomal_table, 2:12, column_names)
     # do some rounding
     rounding <- names(chromosomal_table)[2:12]
-    chromosomal_table[,(rounding) := round(.SD, r), .SDcols=rounding]
-    
+    chromosomal_table[, (rounding) := round(.SD, r), .SDcols = rounding]
+
     return(chromosomal_table)
   }
   # add column of group size
   windows[, group := start %/% group_size]
-  
+
   # aggregate using function
-  grouped <- windows[, .(fun(get(column_names[1])), 
-                         fun(get(column_names[2])),
-                         fun(get(column_names[3])),
-                         fun(get(column_names[4])),
-                         fun(get(column_names[5])),
-                         fun(get(column_names[6])),
-                         fun(get(column_names[7])),
-                         fun(get(column_names[8])),
-                         fun(get(column_names[9])),
-                         fun(get(column_names[10])),
-                         fun(get(column_names[11]))), 
-                     by = .(ID, group)]
-  
+  grouped <- windows[, .(
+    fun(get(column_names[1])),
+    fun(get(column_names[2])),
+    fun(get(column_names[3])),
+    fun(get(column_names[4])),
+    fun(get(column_names[5])),
+    fun(get(column_names[6])),
+    fun(get(column_names[7])),
+    fun(get(column_names[8])),
+    fun(get(column_names[9])),
+    fun(get(column_names[10])),
+    fun(get(column_names[11]))
+  ),
+  by = .(ID, group)
+  ]
+
   # make bed like columns
   grouped[, `:=`(start = group * group_size, end = (group + 1) * group_size, group = NULL)]
-  
-  setcolorder(grouped, c("ID", 
-                         "start", 
-                         "end", 
-                         "V1",
-                         "V2",
-                         "V3",
-                         "V4",
-                         "V5",
-                         "V6",
-                         "V7",
-                         "V8",
-                         "V9",
-                         "V10",
-                         "V11"))
+
+  setcolorder(grouped, c(
+    "ID",
+    "start",
+    "end",
+    "V1",
+    "V2",
+    "V3",
+    "V4",
+    "V5",
+    "V6",
+    "V7",
+    "V8",
+    "V9",
+    "V10",
+    "V11"
+  ))
   setnames(grouped, 4:14, column_names)
 
   rounding <- names(grouped)[4:14]
-  grouped[,(rounding) := round(.SD, r), .SDcols=rounding]
-  
+  grouped[, (rounding) := round(.SD, r), .SDcols = rounding]
+
   return(grouped)
 }
 
-res <- calc_aggregations(file = File, 
-                         window_size = window_size, 
-                         group_size = group_size, 
-                         fun = fun, 
-                         chromosomal = chromosomal)
+##########
+## Main ##
+##########
+
+res <- calc_aggregations(
+  file = File,
+  window_size = window_size,
+  group_size = group_size,
+  fun = fun,
+  chromosomal = chromosomal
+)
 
 fwrite(x = res, file = "", sep = "\t")
